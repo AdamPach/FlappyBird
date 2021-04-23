e34be5c023c88f47017c949c224166e49b52e599
AdamPach <adampach9@gmail.com>
2021-04-23 13:58:29 +0200

    Add moving walls

--- game.py
+++ game.py
@@ -1,6 +1,9 @@
 import pygame
 import os
+import random
 import birdy
+import wally
+import time
 
 pygame.init()
 
@@ -10,20 +13,32 @@ WINDOW_WIDTH = 288
 WINDOW = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("FlappyBird")
 
-FPS = 30
+FPS = 60
 
+#Import Assets
 BACKGROUND = pygame.image.load(os.path.join("assets", "bg.png"))
 BIRD_ASSETS = pygame.image.load(os.path.join("assets", "bird2.png"))
+BOTTOM_WALL_ASSET = pygame.image.load(os.path.join("assets", "pipe.png"))
+TOP_WALL_ASSET = pygame.image.load(os.path.join("assets", "pipe_down.png"))
 
+#Bird Constants
 BIRD_WIDTH = 34
 BIRD_HEIGHT = 24
 
+#Wall Constants
+WALL_FREE_SPACE = 120
+
 BIRD_COLIDE_WALL = pygame.USEREVENT + 1
+
+
+RED = (186, 43, 43)
 
 def game():
     clock = pygame.time.Clock()
     run = True
     bird = birdy.Bird(WINDOW_HEIGHT, BIRD_WIDTH, BIRD_HEIGHT)
+    walls = [wally.Wall(random.randint(50,280), WALL_FREE_SPACE, 170), wally.Wall(random.randint(50,280) , WALL_FREE_SPACE, 340),]
+    #  time.sleep(3)
     while run:
         clock.tick(FPS)
         for event in pygame.event.get():
@@ -35,18 +50,31 @@ def game():
                 bird.jump()
         birdImage = pygame.transform.rotate(BIRD_ASSETS, bird.getRotate())
         bird.gravity()
+        wallsMove(walls)
         controlBird(bird)
         for event in pygame.event.get():
             if event.type == BIRD_COLIDE_WALL:
                 run = False
-        draw(bird, birdImage)
+        draw(bird, birdImage, walls)
 
 
-def draw(bird, birdImage):
+def draw(bird, birdImage, walls):
     birdRect = bird.getBirdRect()
+    #wallTopRect, wallBottRect = wall.getWallRects()
     WINDOW.blit(BACKGROUND, (0, 0))
+   # WINDOW.blit(TOP_WALL_ASSET, (wallTopRect.x, wallTopRect.y))
+    #pygame.draw.rect(WINDOW, RED, wallTopRect)
+    #WINDOW.blit(BOTTOM_WALL_ASSET, (wallBottRect.x, wallBottRect.y))
+    drawWalls(walls)
     WINDOW.blit(birdImage, (birdRect.x,birdRect.y))
     pygame.display.update()
+
+def drawWalls(walls):
+    for wall in walls:
+        wallTopRect, wallBottRect = wall.getWallRects()
+        pygame.draw.rect(WINDOW, RED, wallTopRect)
+        WINDOW.blit(BOTTOM_WALL_ASSET, (wallBottRect.x, wallBottRect.y))
+
 
 def controlBird(bird):
     birdRect = bird.getBirdRect()
@@ -54,3 +82,7 @@ def controlBird(bird):
         pygame.event.post(pygame.event.Event(BIRD_COLIDE_WALL))
     elif birdRect.y <= 0:
         pygame.event.post(pygame.event.Event(BIRD_COLIDE_WALL))
+
+def wallsMove(walls):
+    for wall in walls:
+        wall.movingWalls()
---
+++ wally.py
@@ -0,0 +1,30 @@
+import pygame
+import random
+
+class Wall:
+
+    def __init__(self, topWallHeight, freeSpace, xPos):
+        self.__topWallHeight = topWallHeight
+        self.__freeSpace = freeSpace
+        self.__X_POS = xPos
+        self.__Y_POS = 0
+        self.__WIDTH = 52
+        self.__Y_BOTT_POS = self.__topWallHeight + self.__freeSpace
+        self.__BOTT_HEIGHT = 512 - self.__Y_BOTT_POS
+
+    def getWallRects(self):
+        return pygame.Rect(self.__X_POS, self.__Y_POS, self.__WIDTH, self.__topWallHeight), pygame.Rect(self.__X_POS, self.__Y_BOTT_POS, self.__WIDTH, self.__BOTT_HEIGHT)
+
+    def movingWalls(self):
+        self.__controlWall()
+        self.__X_POS -= 5
+
+    def __regenWall(self):
+        self.__topWallHeight = random.randint(50,280)
+        self.__X_POS = 300
+        self.__Y_BOTT_POS = self.__topWallHeight + self.__freeSpace
+        self.__BOTT_HEIGHT = 512 - self.__Y_BOTT_POS
+
+    def __controlWall(self):
+        if self.__X_POS < 0 - self.__WIDTH:
+            self.__regenWall()
